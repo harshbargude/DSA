bb73ba261657db15bbad43053c41f965c068bf56
Harsh <harshbargude03@gmail.com>
2024-03-07 20:25:30 +0530

    ok

--- Array/Arrays.java
+++
@@ -1,7 +0,0 @@
-package Array;
-
-public class Arrays {
-
-    
-    
-}
---
+++ Array/ArraysDSA.java
@@ -0,0 +1,13 @@
+package Array;
+
+import java.util.Arrays;
+
+public class ArraysDSA {
+
+    public static void main(String[] args) {
+        int[] a = {3,2,6,4,9,1,7};
+
+        Arrays.sort(a);
+    }    
+    
+}
