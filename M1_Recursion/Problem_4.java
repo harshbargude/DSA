0cf624e1ca7ee828de3ee41971c5c61f4b0b8672
Harsh <harshbargude03@gmail.com>
2024-03-08 21:01:50 +0530

    Q4 done

--- M1_Recursion/Problem_4.java
+++ M1_Recursion/Problem_4.java
@@ -1,14 +1,32 @@
 package M1_Recursion;
 
+import java.util.Scanner;
+
 public class Problem_4 {
 
     public static void main(String[] args) {
         // Q4. PRint 1 to N (But not to use i+1)
         // with the Magic of back tracking
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Nummber: ");
+        int N = input.nextInt();
+
+        print1_to_N(N);
+
+
+
+    }
+
+    private static void print1_to_N(int N) {
+
+        if(N == 0){
+            return;
+        }
+
+        print1_to_N(N-1);
+        System.err.println(N);
         
-
-
     }
     
 }
