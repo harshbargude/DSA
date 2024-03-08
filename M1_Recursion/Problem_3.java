73a5ee480556b4251714aeee24a22fe6d6e43425
Harsh <harshbargude03@gmail.com>
2024-03-08 20:27:43 +0530

    Problem 3, print from Nto 1 using Recursion

---
+++ M1_Recursion/Problem_3.java
@@ -0,0 +1,29 @@
+package M1_Recursion;
+
+import java.util.Scanner;
+
+public class Problem_3 {
+
+    public static void main(String[] args) {
+        // Q3. Print in terms of N to 1
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter The Number:");
+
+        int N = input.nextInt();
+        printN_to_1(N);
+
+        input.close();
+    }
+
+    private static void printN_to_1(int N) {
+
+        if (N == 0) {
+            return;
+        }
+        System.out.println(N);
+
+        printN_to_1(N - 1);
+    }
+
+}
