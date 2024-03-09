d324e742dcebb81a69bfc37c8a067cf7d75fd26c
Harsh <harshbargude03@gmail.com>
2024-03-09 07:34:18 +0530

    Parameterised_Way recursion(sum of n number)✔

--- M1_Recursion/Re3/Parameterised_Way.java
+++ M1_Recursion/Re3/Parameterised_Way.java
@@ -4,24 +4,18 @@ import java.util.Scanner;
 
 public class Parameterised_Way {
     public static void main(String[] args) {
-
-// Sum of N Numbers...
-
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter Value of N: ");
+        System.out.print("Enter N:");
         int N = input.nextInt();
-
-        System.out.println(SumofN_Number(N));
-        // SumofN_Number(N,0);
+        sum(N,0);
         input.close();
     }
 
-    private static int SumofN_Number(int n) {
-
-        if (n == 0) {
-            return 0;
+    private static void sum(int n, int i) {
+        if (n < 1) {
+            System.err.println(i);
+            return;
         }
-        return n + SumofN_Number(n-1);
-        
+        sum(n-1,i+n);
     }
 }
