527f4c478e38d9a522f0e4e629b6368961ece465
Harsh <harshbargude03@gmail.com>
2024-03-10 21:14:43 +0530

    Count a perticuler character in String ✔

--- M1_RecursionHashing/Hashing/countCharInStr.java
+++ M1_RecursionHashing/Hashing/countCharInStr.java
@@ -3,16 +3,15 @@ package M1_RecursionHashing.Hashing;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class CountOfNumberInArray {
+public class countCharInStr {
     public static void main(String[] args) {
         // int[] arr = { 2, 4, 3, 6, 4, 5, 8, 32, 4, 1, 54, 2, 3, 5, 4 };
         String strArr = "abcdefgmmvkznbcvsgbzjsdchgacwctrdcasdic";
         HashMap<Character, Integer> hash = new HashMap<>();
-        for (int i = 0; i<strArr.length(); i++) {
+        for (int i = 0; i < strArr.length(); i++) {
             char ele = strArr.charAt(i);
             hash.put(ele, hash.getOrDefault(ele, 0) + 1);
         }
-
 
         Scanner input = new Scanner(System.in);
         // System.out.print("Enter Number TO Search it's occurrences: ");
@@ -21,14 +20,14 @@ public class CountOfNumberInArray {
         char[] cA = searchnumber.toCharArray();
         char SC = cA[0];
 
-        if(hash.containsKey(SC)){
+        if (hash.containsKey(SC)) {
             System.out.println("Character " + SC + " occurs " + hash.get(SC) + " times.");
-        }else{
+        } else {
             System.out.println("Character " + SC + " does not exist in the array.");
         }
 
         System.out.println(hash);
-        
+
         input.close();
     }
 }
