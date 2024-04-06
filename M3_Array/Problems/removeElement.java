865309cf9925fc2163f84a8d3428c4ba0b8a7561
Harsh <harshbargude03@gmail.com>
2024-04-06 16:33:14 +0530

    done✔

--- M3_Array/Problems/removeElement.java
+++ M3_Array/Problems/removeElement.java
@@ -2,21 +2,24 @@ package M3_Array.Problems;
 
 public class removeElement {
     public static void main(String[] args) {
-        int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+        int[] nums = {3,2,2,3};
         int num = removeElement1(nums, 3);
+        System.out.println(num);
     }
 
     public static int removeElement1(int[] nums, int val) {
-        int[] temp = { nums.length };
-        int i =0;
-        for (int ele : nums) {
-            System.out.println(temp[i]);
-            i++;
-            if (ele == val) {
-                ele++;
+        int i = 0;
+        int count = 0;
+        for (int ele = 0; ele < nums.length; ele++) {
+            if (nums[ele] == val) {
+                continue;
+
             }
+            nums[i] = nums[ele];
+            i++;
+            count++;
         }
-        return 3;
+        return count;
     }
 
 }
